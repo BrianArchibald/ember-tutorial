dbe4a86c24a8d9affc9a38107786a42d1176f641
Brian Archibald <brian.archibald@imtapps.com>
2019-04-11 13:19:47 -0500

    added static rentals page and tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -11,12 +11,20 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function (assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
 
   test('should filter the list of rentals by city.', async function (assert) {
